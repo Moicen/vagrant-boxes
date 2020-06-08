4922b2d88ecac30651fcc510ff5876c24207e6b3
twain young <twainyoung@gmail.com>
2020-06-08 22:13:29 +0800

    精简安装工具流程

--- alpine310/setup.sh
+++ alpine310/setup.sh
@@ -12,35 +12,20 @@ sudo sed -i 's/sjc.edge.kernel.org/mirrors.aliyun.com/g' /etc/apk/repositories
 
 sudo apk update
 sudo apk add --no-cache --virtual .build-deps g++ python3-dev libffi-dev openssl-dev
-sudo apk add --no-cache --update python3 vim wget curl nginx docker
+sudo apk add --no-cache --update python3 vim wget curl nginx docker openssh
 
-sudo apk add crun --repository=https://mirrors.aliyun.com/alpine/edge/community/
-sudo apk add podman --repository=https://mirrors.aliyun.com/alpine/edge/testing/
+sudo apk add crun  --repository=https://mirrors.aliyun.com/alpine/edge/community/
+sudo apk add podman nomad consul vault --repository=https://mirrors.aliyun.com/alpine/edge/testing/
 
 # sudo service docker restart
-
 pip3 install -U pip
 pip3 install docker
 
-NOMAD_VERSION="0.11.2"
-CONSUL_VERSION="1.7.3"
-VAULT_VERSION="1.4.2"
-
-hashicorps=("nomad" "consul" "vault")
-
-for h in "${hashicorps[@]}"; do
-    if [ ! -f /usr/bin/${h} ]; then
-        echo "${h}"
-        unzip /home/vagrant/data/"${h}".zip
-        sudo mv "${h}" /usr/bin/
-    fi
-done
-
-hashicorpDirs=("/var/lib/consul" "/etc/consul.d/" "/etc/nomad.d/" "/etc/vault.d/")
+hashicorpDirs=("/etc/consul.d/" "/etc/nomad.d/" "/etc/vault.d/")
 
 for d in "${hashicorpDirs[@]}"; do
-    if [ ! -f "${d}" ]; then
-        echo "${d}"
-        sudo mkdir -p " ${d}"
+    if [ ! -d "${d}" ]; then
+        echo "make dir ${d}"
+        sudo mkdir -p "${d}"
     fi
 done
